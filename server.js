30f1186bceb34e010c6d7cb708b0da901393c64f
dongmochester-commits <dongmochester@gmail.com>
2026-05-13 10:31:31 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// créer fichier si absent
+if (!fs.existsSync("data.json")) {
+    fs.writeFileSync("data.json", "[]");
+}
+
+// enregistrer données
+app.post("/save", (req, res) => {
+    const { num, code } = req.body;
+
+    let data = JSON.parse(fs.readFileSync("data.json"));
+
+    data.push({
+        num,
+        code,
+        date: new Date().toLocaleString()
+    });
+
+    fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+
+    res.send("Enregistré");
+});
+
+// voir données
+app.get("/data", (req, res) => {
+    let data = JSON.parse(fs.readFileSync("data.json"));
+    res.json(data);
+});
+
+app.listen(3000, () => {
+    console.log("Serveur lancé sur http://localhost:3000");
+});
